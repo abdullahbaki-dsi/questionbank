5592499ce6276a8be2ee72fc31eef36f1c5d2f78
abdullabaki-dsi <abdullahbaki.dsi@gmail.com>
2024-04-23 05:35:23 +0600

    adding dtos

--- approvalService/src/main/java/org/dsi/com/approvalService/dto/ApprovalDTO.java
+++ approvalService/src/main/java/org/dsi/com/approvalService/dto/ApprovalDTO.java
@@ -1,4 +1,14 @@
 package org.dsi.com.approvalService.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ApprovalDTO {
 }
--- approvalService/src/main/java/org/dsi/com/approvalService/model/ApprovalStep.java
+++ approvalService/src/main/java/org/dsi/com/approvalService/model/ApprovalStep.java
@@ -1,4 +1,13 @@
 package org.dsi.com.approvalService.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApprovalStep {
 }
